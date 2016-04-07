52404966dc8b3f387cd0f3af4a8d0009baf2f8dc
Ankur Sharma <ankur.sharma5102@gmail.com>
2016-04-07 10:45:42 +0530

    Added MyConnection class for getting Connection for JDBC and some modifications in JDBC_Conn.java file for getting connection

--- JDBC_Conn.java
+++ JDBC_Conn.java
@@ -7,27 +7,50 @@ import java.sql.Timestamp;
 
 public class JDBC_Conn 
 {
-    /**Static block is executed when a class is loaded into memory
-     * This Block loads MySQL's JDBC driver d */
-    
-    static
-    {
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-        }
-        catch(ClassNotFoundException cnf)
-        {
-            System.out.println("Driver Not Found :"+cnf);
-        }
-    }
-
-        void insert(String roll_num, 
+    void insertStudent(String roll_num, 
                          String first_name, 
                          String last_name, 
                          String branch,
                          String phone,
                          String email){
+          
+         /* String connectionUrl = "jdbc:mysql://localhost:3306/student_info";
+          String dbUser = "root";
+          String dbPwd = "sharma";
+          Connection conn;
+          */
+          ResultSet rs;
+          
+         String queryString = "insert into students values("+"'"+roll_num+"'"+", '"+first_name+"' , '"+last_name+"' , '"+branch+"' , '"+phone+"' , '"+email+"' , CURRENT_TIMESTAMP());";
+        
+        // System.out.println(queryString);
+ 
+        try
+        {
+            //conn = DriverManager.getConnection(connectionUrl, dbUser, dbPwd);
+            MyConnection connector = new MyConnection("mysql","localhost",3306,"student_info","root","sharma");
+
+            Connection conn = connector.getConn();
+            Statement stmt = conn.createStatement();
+       
+            stmt.executeUpdate(queryString);
+       
+            if(conn != null) 
+            {   conn.close(); 
+                conn = null; 
+            }
+        }
+        catch(SQLException sqle)
+        {
+            System.out.println("SQL Exception thrown: "+sqle);
+        }
+    }
+
+   void insertTeacher(String uname,
+                        String pass,
+                        String teacher_name,
+                        String branch,
+                        String subject){
           
           String connectionUrl = "jdbc:mysql://localhost:3306/student_info";
           String dbUser = "root";
@@ -35,9 +58,9 @@ public class JDBC_Conn
           Connection conn;
           ResultSet rs;
           
-         String queryString = "insert into students values("+"'"+roll_num+"'"+", '"+first_name+"' , '"+last_name+"' , '"+branch+"' , '"+phone+"' , '"+email+"' , CURRENT_TIMESTAMP());";
+         String queryString = "insert into teacher values(1,"+"'"+uname+"'"+", '"+pass+"' , '"+teacher_name+"' , '"+branch+"' , '"+subject+"');";
         
-        // System.out.println(queryString);
+         System.out.println(queryString);
  
         try
         {
@@ -56,12 +79,14 @@ public class JDBC_Conn
         {
             System.out.println("SQL Exception thrown: "+sqle);
         }
+
     }
 
     public static void main(String[] args) 
     {
         JDBC_Conn j = new JDBC_Conn();
-        j.insert("186","kinshuk","sarabhai","cse","9989898020","kinshuksarabhai@gmail.com");
+        j.insertStudent("186","kinshuk","sarabhai","cse","9989898020","kinshuksarabhai@gmail.com");
+        j.insertTeacher("mohit","pass","mohit narang","ece","Digital Electronics");
     }
 
 }
---
+++ MyConnection.java
@@ -0,0 +1,55 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MyConnection{
+
+    /**Static block is executed when a class is loaded into memory
+     * This Block loads MySQL's JDBC driver d */
+
+    static
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch(ClassNotFoundException cnf)
+        {
+            System.out.println("Driver Not Found :"+cnf);
+        }
+    }
+
+
+    String db, host, dbname, dbUser, dbPwd;
+    int port;
+
+    MyConnection(String db,
+            String host, 
+            int port, 
+            String dbname,
+            String dbUser,
+            String dbPwd) {
+        this.db =db;
+        this.host = host;
+        this.dbname = dbname;
+        this.port = port;
+        this.dbUser = dbUser;
+        this.dbPwd = dbPwd;
+    }
+
+    Connection getConn() throws SQLException{
+        String connectionUrl = String.format("jdbc:%s://%s:%d/%s",
+                db, host, port, dbname); 
+        Connection conn = DriverManager.getConnection(connectionUrl,dbUser,dbPwd);
+        return conn;
+    }
+
+    public static void main(String[] args){
+
+        // Connection n = new Connection();
+        //  n.getConn("mysql","localhost","3306","student_info");
+    }
+}
+
