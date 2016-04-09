434f301e3e442231408d9595d58d5e75b2f8a610
Ankur Sharma <ankur.sharma5102@gmail.com>
2016-04-09 14:09:07 +0530

    prepared statement used to get values into database

--- JDBC_Conn.java
+++ JDBC_Conn.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
+import java.sql.*;
 
 public class JDBC_Conn 
 {
@@ -18,22 +18,22 @@ public class JDBC_Conn
                          String first_name, 
                          String last_name, 
                          String branch,
-                         String phone,
-                         String email){
-          
-         ResultSet rs;
-          
-         String queryString = "insert into student values("+"'"+roll_num+"'"+", '"+pass+"' , '"+first_name+"' , '"+last_name+"' , '"+branch+"' , '"+phone+"' , '"+email+"' , CURRENT_TIMESTAMP());";
-        
-         System.out.println(queryString);
- 
-        try
+                         String phone_num,
+                         String email) {
+       try
         {
-            Connection conn = connector.getConn();
-            Statement stmt = conn.createStatement();
-       
-            stmt.executeUpdate(queryString);
-       
+           Connection conn = connector.getConn();
+           PreparedStatement updatemem = conn.prepareStatement("insert into student(roll_num, pass, first_name, last_name, branch, phone_num, email) values(?,?,?,?,?,?,?)");
+        updatemem.setString(1,roll_num);
+        updatemem.setString(2,pass);
+        updatemem.setString(3,first_name);
+        updatemem.setString(4,last_name);
+        updatemem.setString(5,branch);
+        updatemem.setString(6,phone_num);
+        updatemem.setString(7,email);
+        updatemem.executeUpdate();
+        updatemem.close();
+
             if(conn != null) 
             {   conn.close(); 
                 conn = null; 
@@ -49,22 +49,22 @@ public class JDBC_Conn
                         String pass,
                         String teacher_name,
                         String branch,
-                        String subject){
+                        String subject) {
           
-         ResultSet rs;
-          
-         String queryString = "insert into teacher (uname, pass, teacher_name, branch, subject) values   ("+"'"+uname+"'"+", '"+pass+"' , '"+teacher_name+"' , '"+branch+"' , '"+subject+"');";
-        
-         System.out.println(queryString);
- 
-        try
+       try
         {
-            Connection conn = connector.getConn();
-            Statement stmt = conn.createStatement();
-       
-            stmt.executeUpdate(queryString);
-       
-            if(conn != null) 
+           Connection conn = connector.getConn();
+           PreparedStatement ps = conn.prepareStatement("insert into teacher (uname, pass, teacher_name, branch, subject) values(?,?,?,?,?)");
+
+               ps.setString(1,uname);
+               ps.setString(2,pass);
+               ps.setString(3,teacher_name);
+               ps.setString(4,branch);
+               ps.setString(5,subject);
+               ps.executeUpdate();
+               ps.close(); 
+               
+               if(conn != null) 
             {   conn.close(); 
                 conn = null; 
             }
@@ -73,13 +73,12 @@ public class JDBC_Conn
         {
             System.out.println("SQL Exception thrown: "+sqle);
         }
-
     }
 
     public static void main(String[] args) 
     {
         JDBC_Conn j = new JDBC_Conn();
-        j.insertStudent("186","meetu","kinshuk","sarabhai","cse","9989898020","kinshuksarabhai@gmail.com");
+        j.insertStudent("186","pass","kinshuk","sarabhai","cse","9989898020","kinshuksarabhai@gmail.com");
         j.insertTeacher("mohit","pass","mohit narang","ece","Digital Electronics");
     }
 
